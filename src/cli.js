76c37eff7a5504c22b7776ddc58903da124eabd0
polinagv <pol.grachewa@yandex.ru>
2022-07-21 23:20:04 +0300

    setup Eslint

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const userGreeting = () => {
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
-}
+const userGreeting = () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
+
+export default userGreeting;
